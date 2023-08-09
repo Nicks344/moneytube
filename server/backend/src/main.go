a6be82aa51bc974711e79adb3bfe7047a090f741
Nicks344 <nicks344@yandex.ru>
2023-08-09 22:42:30 +0400

    sa
    as

--- server/backend/src/main.go
+++ server/backend/src/main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 
 	"github.com/meandrewdev/logger"
-	"github.com/meandrewdev/moneytube/server/backend/src/config"
-	"github.com/meandrewdev/moneytube/server/backend/src/model"
-	"github.com/meandrewdev/moneytube/server/backend/src/server"
+	"github.com/Nicks344/moneytube/server/backend/src/config"
+	"github.com/Nicks344/moneytube/server/backend/src/model"
+	"github.com/Nicks344/moneytube/server/backend/src/server"
 )
 
 func main() {
